141d1336a7f337658ffcb47e98b042846e0cf37a
Jan Tojnar <jtojnar@gmail.com>
2025-03-02 23:59:08 +0000

    vte: 0.78.4 → 0.79.91
    https://gitlab.gnome.org/GNOME/vte/-/compare/0.78.4...0.79.91

--- pkgs/by-name/vt/vte/package.nix
+++ pkgs/by-name/vt/vte/package.nix
@@ -34,7 +34,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "vte";
-  version = "0.78.4";
+  version = "0.79.91";
 
   outputs = [
     "out"
@@ -43,7 +43,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = "mirror://gnome/sources/vte/${lib.versions.majorMinor finalAttrs.version}/vte-${finalAttrs.version}.tar.xz";
-    hash = "sha256-LepOQSJmWStkYKP+RIj149UHEvE5gVeQwOy0RxD34X4=";
+    hash = "sha256-rcpmfUCuGDm6JYtjcBzQX7okkwPXIlhxHMaSlLLLhkY=";
   };
 
   patches = [
